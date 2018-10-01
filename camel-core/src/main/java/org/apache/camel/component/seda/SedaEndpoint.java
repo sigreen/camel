672c81746520376ab350474af2b36aec958b6a0f
Guillaume Nodet <gnodet@gmail.com>
2018-10-01 17:20:26 +0200

    Use a single field + lock to manage the services state

--- camel-core/src/main/java/org/apache/camel/component/seda/SedaEndpoint.java
+++ camel-core/src/main/java/org/apache/camel/component/seda/SedaEndpoint.java
@@ -510,7 +510,7 @@ public class SedaEndpoint extends DefaultEndpoint implements AsyncEndpoint, Brow
 
     @Override
     public void shutdown() throws Exception {
-        if (shutdown.get()) {
+        if (isShutdown()) {
             log.trace("Service already shut down");
             return;
         }
